2ddf3c9ce1f6ae8335030a95ac39699a5514a2e0
Giovanni Muniz <tul40930@temple.edu>
2025-11-19 11:38:20 -0500

    added files
    added sql, and diagram images

---
+++ GroupProjectP3_Team4.sql
@@ -0,0 +1,58 @@
+CREATE TABLE Stay
+(
+    StayID          NUMBER UNIQUE,
+    StartTime       TIMESTAMP,
+    EndTime         TIMESTAMP,
+    PatientID       NUMBER,
+    PhysicianID     NUMBER,
+    CONSTRAINT Stay_pk PRIMARY KEY (StayID)
+);
+
+CREATE TABLE Nurse
+(
+    NurseID     NUMBER UNIQUE,
+    FirstName   VARCHAR(50) NOT NULL,
+    LastName    VARCHAR(50) NOT NULL,
+    DOB         DATE NOT NULL,
+    StayID      NUMBER,
+    CONSTRAINT Nurse_pk PRIMARY KEY (NurseId)
+);
+
+CREATE TABLE Department
+(
+    DepartmentID    NUMBER UNIQUE,
+    FloorNum        NUMBER,
+    DeptName        VARCHAR(50) NOT NULL,
+    NurseID         NUMBER,
+    PhysicianID     NUMBER,
+    CONSTRAINT Department_pk PRIMARY KEY (DepartmentID)
+);
+
+CREATE TABLE Physician
+(
+    PhysicianID        NUMBER UNIQUE,
+    FirstName          VARCHAR(50) NOT NULL,
+    LastName           VARCHAR(50) NOT NULL,
+    DOB                DATE NOT NULL,
+    CONSTRAINT Physician_pk PRIMARY KEY (PhysicianID)
+);
+
+CREATE TABLE Patient
+(
+    PatientID           NUMBER UNIQUE,
+    FirstName           VARCHAR(50) NOT NULL,
+    LastName            VARCHAR(50) NOT NULL,
+    DOB                 DATE NOT NULL,
+    InsuranceID         NUMBER,
+    CONSTRAINT Patient_pk PRIMARY KEY (PatientID)
+);
+
+CREATE TABLE Medication 
+(
+    MedicationID          NUMBER UNIQUE,
+    Brand                 VARCHAR(50),
+    Description           VARCHAR(100),
+    Name                  VARCHAR(50),
+    CONSTRAINT Medication_pk PRIMARY KEY (MedicationID)
+);
+
